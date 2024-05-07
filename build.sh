f7ae5e92560fecc0c5b92a87efe2eefd586559e8
Luiz2002 <quineche44@gmail.com>
2024-05-07 16:14:59 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations 
 python manage.py migrate
 if [ "$CREATE_SUPERUSER" = "true" ]; then
     export DJANGO_SUPERUSER_USERNAME="admin"
