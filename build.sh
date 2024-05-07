7723663a5b0f3579f95ff8065102c65c9b8dc2d6
Luiz2002 <quineche44@gmail.com>
2024-05-07 14:55:47 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+    echo "Creating superuser..."
+    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com'), getenv('DJANGO_SUPERUSER_PASSWORD', '123'))" | python manage.py shell
+fi
