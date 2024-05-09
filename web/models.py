6eab019ee4f7d198317bf477ad161679d3062be4
Luiz2002 <quineche44@gmail.com>
2024-05-09 10:14:57 -0500

    Update models.py

--- web/models.py
+++ web/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-#from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group
 from django.contrib.auth.hashers import make_password
 from configuracion.models import *
 
