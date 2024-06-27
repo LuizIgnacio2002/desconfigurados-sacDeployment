5f06efa7d2d0443a3195151c498cc790dc7f85dc
Luiz2002 <quineche44@gmail.com>
2024-06-27 16:26:38 +0000

    django corsheaders added

--- backend/settings.py
+++ backend/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # corsheaders
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
