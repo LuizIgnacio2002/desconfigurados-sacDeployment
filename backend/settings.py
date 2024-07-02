ea1de08b0397fbc9ca105769faa392b6c13fde1e
Jorge Ktch <jorgektch@gmail.com>
2024-07-01 20:37:20 -0500

    Actualizar permisos

--- backend/settings.py
+++ backend/settings.py
@@ -154,9 +154,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        'rest_framework.permissions.IsAdminUser'    ]
 }
 
 # Para todas las conexiones
